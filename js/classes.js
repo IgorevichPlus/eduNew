f46b638dc40e20cd3c1ab6d61e1c6f376b3fe564
Teo Nys <teoigorovich@gmail.com>
2017-06-04 13:52:57 +0000

    updating classes

--- js/classes.js
+++ js/classes.js
@@ -1,13 +1,14 @@
 // getting data from firebase
+window.classesList = {1:{title:"imgay", description:"i'mstraight;"}};
 var classesData = firebase.database().ref('classes/');
-var classes = {};
 classesData.on('value', function(snapshot) {
-    classes = snapshot.val();
+    window.classesList = snapshot.val();
+    console.log(snapshot.val());
 });
 
 var div = document.getElementById('enterdata');
 
-
+ console.log(JSON.stringify(classes, null, 2));
 classes.forEach(function(element){
   div.innerHTML = div.innerHTML + JSON.stringify(element, null, 2);
 });
--- js/dashboard.js
+++ js/dashboard.js
@@ -10,15 +10,28 @@ firebase.auth().onAuthStateChanged(function(user) {
 			document.getElementById("signout").innerHTML += " " + ds.val();
 			document.getElementById("user_name").innerHTML += " " + ds.val();
 		});
+		firebase.database().ref("users/" + toFirebaseFormat(firebase.auth().currentUser.uid) + "/classes" +"/registered").once("value").then(function(dc) {
+			
+			firebase.database().ref("classes/" + dc.val() + "/title").once("value").then(function(dt){
+				document.getElementById("class-title").innerHTML += " " + dt.val();
+				
+			});
+			
+			firebase.database().ref("classes/" + dc.val() + "/teacher").once("value").then(function(dts){
+				firebase.database().ref("users/" + toFirebaseFormat(dts.val()) + "/username").once("value").then(function(dn) {
+					document.getElementById("class-teacher").innerHTML += " " + dn.val();
+				});
+				
+			});
+			
+			firebase.database().ref("classes/" + dc.val() + "/description").once("value").then(function(dd){
+				document.getElementById("class-description").innerHTML += " " + dd.val();
+				
+			});
+		});
 		document.getElementById("user_email").innerHTML += " " + firebase.auth().currentUser.email;
 		
 
-		
-		
-		
-		
-
-		
 	} else window.location.replace("index.html");
 
 });
