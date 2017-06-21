5c6f4c85fac3f5a13208c331e5a565e1ca7aa17e
Teo Nys <teoigorovich@gmail.com>
2017-06-20 20:18:21 -0400

    updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -10,55 +10,58 @@ firebase.auth().onAuthStateChanged(function(user) {
 			document.getElementById("signout").innerHTML += " " + ds.val();
 			document.getElementById("user_name").innerHTML += " " + ds.val();
 		});
-		
-		
+
+
 		firebase.database().ref("users/" + toFirebaseFormat(firebase.auth().currentUser.uid) + "/classes" +"/registered").once("value").then(function(dc) {
-			
+
 			firebase.database().ref("classes/" + dc.val() + "/title").once("value").then(function(dt){
 				document.getElementById("class-title").innerHTML += " " + dt.val();
-				
+
 			});
-			
+
 			firebase.database().ref("classes/" + dc.val() + "/teacher").once("value").then(function(dts){
 				firebase.database().ref("users/" + toFirebaseFormat(dts.val()) + "/username").once("value").then(function(dn) {
 					document.getElementById("class-teacher").innerHTML += " " + dn.val();
 				});
-				
+
 			});
-			
+
 			firebase.database().ref("classes/" + dc.val() + "/description").once("value").then(function(dd){
 				document.getElementById("class-description").innerHTML += " " + dd.val();
-				
+
 			});
 		});
-		
+
 		firebase.database().ref("users/" + toFirebaseFormat(firebase.auth().currentUser.uid) + "/classes" +"/registered").once("value").then(function(rg) {
+
 			rg.val().forEach(function(x){
-				var xu;
-				var dx;
+				var users = "fake";
+				var dess = "fake";
+				var title = "fake";
+
 			  firebase.database().ref("classes/" + x + "/teacher").once("value").then(function(nmd){
-			  	
-			  	firebase.database().ref("users/" + nmd.val() + "/username").once("value").then(function(xd){
-			  		xu = xd.val();
-			  	});
-			  	
-			  });
-			  firebase.database().ref("classes/" + x + "/description").once("value").then(function(desc){
-					dx = desc.val()
-				
+					firebase.database().ref("users/" + toFirebaseFormat(nmd.val() ) + "/username").once("value").then(function(udd){
+						users = udd.val();
+					});
+				});
+				firebase.database().ref("classes/" + x + "/description").once("value").then(function(dd){
+						dess = dd.val();
+				});
+				firebase.database().ref("classes/" + x + "/title").once("value").then(function(tt){
+						title = tt.val();
+				});
+			addTo(title, dess, users);
 			});
-			addTo(xu, dx, "NIkolay");
-			});
-			
-			
+
+
 		});
-		
+
 		document.getElementById("user_email").innerHTML += " " + firebase.auth().currentUser.email;
-		
+
 
 	} else window.location.replace("index.html");
 
 });
 var addTo = function(title, description, teacher){
-console.log(title + " " + description + " " + teacher);
+	document.getElementById("classlist").innerHTML += " "  + title + " "  + description + " " + teacher;
 }
